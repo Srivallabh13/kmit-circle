1889ce78cc66d0673ff587fbcd59395313342603
Srivallabh13 <srivallabhjoshi13@gmail.com>
2023-10-06 21:38:38 +0530

    last commit

--- server/server.js
+++ server/server.js
@@ -29,7 +29,11 @@ config({ path: '/.env' })
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.json({ limit: '10mb' }));
-app.use(cors())
+app.use(cors({
+    credentials:true,
+    origin:'https://campuscircle17.netlify.app/',
+}))
+
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 const storage = multer.diskStorage({
