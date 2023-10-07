9f5ddb9ceb176f3779d220189e57f58309089e3d
Srivallabh13 <srivallabhjoshi13@gmail.com>
2023-10-07 12:22:41 +0530

    testing phase 3

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -17,7 +17,7 @@ export const login = async (req, res, next) => {
             return next(new ErrorHandler("Invalid Password", 400));
         }
         
-        const token = jwt.sign({_id:user._id}, process.env.JWT_SECRET);
+        const token = await jwt.sign({_id:user._id}, process.env.JWT_SECRET);
         res.status(200).cookie("token", token, {
             httpOnly: true,
             secure: true,
