f8899bf1050de03228b73f95e114a4a91daf960d
Srivallabh13 <srivallabhjoshi13@gmail.com>
2023-10-07 12:36:33 +0530

    testing phase 3

--- client/src/Reducers/User.js
+++ client/src/Reducers/User.js
@@ -43,17 +43,19 @@ export const userReducer = createReducer(initialState, (builder) => {
       state.loading = false;
       state.error = action.payload;
     })
-
+    
     .addCase("LoadUserRequest", (state) => {
       state.loading = true;
     })
     .addCase("LoadUserSuccess", (state, action) => {
       state.loading = false;
       state.user = action.payload;
+      state.isAuthenticated = true;
     })
     .addCase("LoadUserFailure", (state, action) => {
       state.loading = false;
       state.error = action.payload;
+      state.isAuthenticated = false;
     })
     
     .addCase("LogoutRequest",(state) => {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,9 @@
 import express from "express";
 import {login, logout} from '../controllers/auth.js'
-import { isAuth } from "../middlewares/auth.js";
 const router = express.Router();
 
 
-router.get('/logout', isAuth, logout);
-router.post('/login', isAuth, login);
+router.get('/logout', logout);
+router.post('/login', login);
 
 export default router
