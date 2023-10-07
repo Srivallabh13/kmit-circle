2dc92fbce9e6c34c679fd18e1b53dd40108a573f
Srivallabh13 <srivallabhjoshi13@gmail.com>
2023-10-08 01:01:12 +0530

    final-stage-17

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,14 +2,16 @@ import User from '../models/User.js'
 import jwt from 'jsonwebtoken'
 import ErrorHandler from '../utils/errorhandler.js'
 // import {LocalStorage} from 'node-localstorage';
+const cookie = require('cookie');
+
+// Assuming req is your request object
 
 // var localStorage = new LocalStorage('./scratch');
 export const isAuth = async(req,res,next) => {
     try {
-        const {new_token} = req.cookies
-        const {token} = req.cookies
+        const cookies = cookie.parse(req.headers.cookie || '');
+        const new_token = cookies.new_token;
         console.log("new token ",new_token);
-        console.log("token ",token);
 
         if(!new_token) {
             return next(new ErrorHandler("Please login first", 401))
